fdfe5131c823d5e8a23bdd13126895982c6af046
Jason  Zhou <jzhou35@buffalo.edu>
2020-11-12 05:00:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,6 +67,11 @@ def runner
   welcome
   initial_round
   hit?(card_total)
-  display_card_total(card_total)
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game
+  
 end
     
