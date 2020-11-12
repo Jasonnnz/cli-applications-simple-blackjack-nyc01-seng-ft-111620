34f5e6daabdd76191ab600b7e4c1c1338b40848e
Jason  Zhou <jzhou35@buffalo.edu>
2020-11-12 05:07:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,8 +65,7 @@ end
 def runner
   # code runner here
   welcome
-  initial_round
-  card_total = sum
+  card_total = initial_round
   while card_total < 21
     card_total = hit?(card_total)
     card_total = display_card_total(card_total)
